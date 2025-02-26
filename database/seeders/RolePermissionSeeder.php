a677e54ae9fe471c158670ca73e9dfcd8d0c1567
Yahya afadisse <yahyaafadisse92@gmail.com>
2025-02-26 10:31:26 +0000

    HRMS-6: fixer la gestion des roles et des permissions avec spatie

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        $permissions = [
+            'create departement',
+            'edit departement',
+            'delete departement',
+            'view departement',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate(['name' => $permission]);
+        }
+
+        $adminRole = Role::firstOrCreate(['name' => 'admin']);
+
+        $adminRole->syncPermissions($permissions);
+
+        $user = \App\Models\User::first();
+        if ($user) {
+            $user->assignRole('admin');
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
